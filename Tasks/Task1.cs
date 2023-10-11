b02f61b054333fd18d8312802a4575ed71d13ef5
JordanGallant <39-12602@sae.edu.za>
2023-10-11 16:00:31 +0200

    adding-commnets

--- Tasks/Task1.cs
+++ Tasks/Task1.cs
@@ -22,6 +22,7 @@ namespace Tasks
         {
             InitializeComponent();
         }
+        //setting timer variables
         private int countdownValue = 0;
         private int stepNumber = 0;
 
@@ -36,16 +37,18 @@ namespace Tasks
             
             if (int.TryParse(textBox1.Text, out int countdownValue) && int.TryParse(textBox2.Text, out int stepNumber))
             {
-
+                //base value for text box on button click
                 richTextBox1.Text = $" Countdown:{countdownValue.ToString()} seconds \n" ;
+                //setting timer varibale to user input
                 countdownValue = int.Parse(textBox1.Text);
-                
+                //starting timer on button click
                 timer1.Start();
 
 
             }
             else
             {
+                //data valadation
                 richTextBox1.Text = "Please enter a valid number."+ " " + "Invalid Input Make sure all fields have been filled in" ;
             }
         }
@@ -65,11 +68,13 @@ namespace Tasks
         {
             if (int.TryParse(textBox1.Text, out int countdownNumber) && int.TryParse(textBox2.Text, out int stepNumber))
             {
-                // Start the countdown
+                // Loop for the method that isnt using a timer
                 for (int i = countdownNumber; i >= 0; i -= stepNumber)
                 {
                     richTextBox1.Text += $"Countdown: {i}" + " " +  "Countdown" + "\n";
+                    //decrement user input by step and use the step value to add delay
                     richTextBox1.Text += $"Wating {stepNumber} seconds " + "\n";
+                    //using asynchronis functions 
                     await Task.Delay(stepNumber*1000);
                     
 
@@ -89,23 +94,27 @@ namespace Tasks
 
         private void button3_Click(object sender, EventArgs e)
         {
+            //clear button mainly used for testing purposes
             richTextBox1.Clear();
             timer1.Stop();
         }
 
         private async void timer1_Tick(object sender, EventArgs e)
         {
+            //setting timer variables to the user input
             countdownValue = int.Parse(textBox1.Text);
             stepNumber = int.Parse(textBox2.Text);
-
+            //wile the timer varibale is not 0
             while (countdownValue > 0)
             {
+                //decrement user input by step and use the step value to add delay
                 countdownValue-= stepNumber;
                 richTextBox1.Text += $" Count Down: {countdownValue}" + " seconds" + "\n";
                 richTextBox1.Text += $"Waiting {stepNumber} seconds" + "\n";
                 await Task.Delay(stepNumber*1000);
                 
             }
+           
             timer1.Stop();
             
 
