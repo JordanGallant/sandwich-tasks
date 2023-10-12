a9ab62a73c161a2b583f58b29ae875653c106690
JordanGallant <39-12602@sae.edu.za>
2023-10-12 11:10:42 +0200

    get-values-for-child-nodes

--- Tasks/Task2.cs
+++ Tasks/Task2.cs
@@ -8,8 +8,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+public class Person
+{
+
+    //declaring public variables for the person object that will be stored in a single tree node.
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+
+
+
+    //person object constructor
+    public Person(string name, string surname, int age, string gender)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+        Gender = gender;
+    }
+
+
+}
+
+class TreeNode
+
+{
+    //Data of the person object that will be stored in each node, name age, gender etc.
+    public Person Data { get; set; }
+    //a list of children will also be stored in each node initialized as an empty array.
+    public List<TreeNode> Children { get; set; }
+
+    //tree node constructor 
+    public TreeNode(Person data)
+    {
+        Data = data;
+        Children = new List<TreeNode>();
+    }
+
+    //this method adds a new child node using the built in array method add.
+    public void AddChild(TreeNode child)
+    {
+        Children.Add(child);
+    }
+    //this method removes a new child node using the built in array method add.
+    public void RemoveChild(TreeNode child)
+    {
+        Children.Remove(child);
+    }
+}
 namespace Tasks
 {
+
+    
     public partial class Task2 : Form
     {
         /* Explination:
@@ -22,55 +73,56 @@ namespace Tasks
          * 
          */
 
-        class Person {
-
-            //declaring public variables for the person object that will be stored in a single tree node.
-            public string Name { get; set; }   
-            public string Surname { get; set; }
-            public int Age { get; set; }
-            public string Gender { get; set; }
-
-            //person object constructor
-            public Person(string name, string surname, int age, string gender)
-            {
-                Name = name;
-                Surname = surname; 
-                Age = age;
-                Gender = gender;
-            }
-
-        
-        }
-
-        class TreeNode
-
-        {
-            //Data of the person object that will be stored in each node, name age, gender etc.
-            public Person Data { get; set; }
-            //a list of children will also be stored in each node initialized as an empty array.
-            public List<TreeNode> Children { get; set; }    
-            
-            //tree node constructor 
-            public TreeNode(Person data) 
-            { 
-                Data = data;
-                Children = new List<TreeNode>();
-            }
-
-            //this method adds a new child to a node
-            public void AddChild(TreeNode child)
-            {
-                Children.Add(child);
-            }
-        }
-
 
         public Task2()
         {
             InitializeComponent();
         }
+        
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            //setting up nodes
+            TreeNode Root = new TreeNode(new Person("Jordan", "Gallant", 22, "Male"));
+
+            TreeNode Parent1 = new TreeNode(new Person("Helen", "Gallant", 57, "Female"));
+            TreeNode Parent2 = new TreeNode(new Person("Solly", "Gallant", 64, "Male"));
+
+            TreeNode GrandparentLeft1 = new TreeNode(new Person("Vic", "Penrith", 69, "Male"));
+            TreeNode GrandparentLeft2 = new TreeNode(new Person("Gill", "Penrith", 70, "Female"));
+
+            TreeNode GrandparentRight1 = new TreeNode(new Person("Peter", "Gallant", 78, "Male"));
+            TreeNode GrandparentRight2 = new TreeNode(new Person("Mary", "Gallant", 76, "Female"));
+
+
+            //hard code family tree
+            // adding Jordans Parents(Child nodes)
+            Root.AddChild(Parent1);
+            Root.AddChild(Parent2);
+            //Adding Helen's Parents(Child nodes)
+            Parent1.AddChild(GrandparentLeft1);
+            Parent1.AddChild(GrandparentLeft2);
+            //Adding Solly's Parents(Child nodes)
+            Parent2.AddChild(GrandparentRight1);
+            Parent2.AddChild(GrandparentRight2);
+
+            DisplayFamilyTree(Root, 0);
+
+
+            
+        }
+           static void DisplayFamilyTree(TreeNode node, int depth)
+        {
+            MessageBox.Show(new string('-', depth*4)+ node.Data.Name + ", " + node.Data.Surname + ", " + node.Data.Age + ", " + node.Data.Gender); // node.Data represents the person object of that node
+            foreach (var child in node.Children)  // O(n) linear time complexity -  looping through each child node (parent) 
+            { 
+                DisplayFamilyTree(child, depth+1); //recursion 
+                // this will display each child node (parent) 
+            }
+        
+        }
+
+        private void Task2_Load(object sender, EventArgs e)
         {
 
         }
