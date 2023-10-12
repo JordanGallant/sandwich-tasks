95e6fed7638f7c49874c66adbb4081068b318f78
JordanGallant <39-12602@sae.edu.za>
2023-10-12 08:01:06 +0200

    added-tree-node-class

--- Tasks/Task2.cs
+++ Tasks/Task2.cs
@@ -12,10 +12,19 @@ namespace Tasks
 {
     public partial class Task2 : Form
     {
+        /* Explination:
+         * Tree data structure that stores a Person Object in a node
+         * Tree data structures are non linear,
+         * in this case we have to use a non linear data structure as we are showing the relationship between heiracal relationships
+         * 
+         * A person will be represented by a tree node and each node will have at least 1 children node
+         * 
+         * 
+         */
 
         class Person {
 
-            //declaring public variables for the person object
+            //declaring public variables for the person object that will be stored in a single tree node.
             public string Name { get; set; }   
             public string Surname { get; set; }
             public int Age { get; set; }
@@ -31,6 +40,28 @@ namespace Tasks
             }
 
         
+        }
+
+        class TreeNode
+
+        {
+            //Data of the person object that will be stored in each node, name age, gender etc.
+            public Person Data { get; set; }
+            //a list of children will also be stored in each node initialized as an empty array.
+            public List<TreeNode> Children { get; set; }    
+            
+            //tree node constructor 
+            public TreeNode(Person data) 
+            { 
+                Data = data;
+                Children = new List<TreeNode>();
+            }
+
+            //this method adds a new child to a node
+            public void AddChild(TreeNode child)
+            {
+                Children.Add(child);
+            }
         }
 
 
