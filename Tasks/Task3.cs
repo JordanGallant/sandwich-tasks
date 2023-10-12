eccafb135d24365a497adf6a9ffb2d1c0e1e1bac
JordanGallant <39-12602@sae.edu.za>
2023-10-12 16:33:13 +0200

    implement Regex

--- Tasks/Task3.cs
+++ Tasks/Task3.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Linq.Expressions;
 
 namespace Tasks
 {
@@ -30,16 +32,26 @@ namespace Tasks
 
         private void button1_Click(object sender, EventArgs e)
         {
-            /* create empty array, break string up push into array, valate if between 1-10, is an integer (Account for whitespace)
-             * otherwise break loop and message show reason for fail
-             * 
-             * 
-             */
+            var numberRegex = new Regex(@"^(10|[1-9])$"); //regular expressions @ - treat expression as is -  \d+ numbers 1-10 and no letters
+            var matches = numberRegex.Matches(textBox1.Text); // creates an array of all integers from 1-10
+
+            foreach ( var match in matches ) //looping through the matches array
+            {
+                if (int.TryParse(match.ToString() , out int num) && num >= 1 && num <= 10)
+                {
+                    CalculationEngine engine = new CalculationEngine();
+                    double result = engine.Calculate(textBox1.Text);
+                    MessageBox.Show("Result: " + result);
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter an Integer between 1-10");
+                }
+            }
 
 
-            CalculationEngine engine = new CalculationEngine();
-                double result = engine.Calculate(textBox1.Text);
-                MessageBox.Show("Result: " + result);
+            
             
         }
     }
