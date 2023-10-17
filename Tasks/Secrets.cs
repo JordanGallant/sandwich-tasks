a54f13d9345723ff3a20f22de4fbeb137cd7830f
JordanGallant <39-12602@sae.edu.za>
2023-10-17 13:22:59 +0200

    adding-secrets

---
+++ Tasks/Secrets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Secrets
+    {
+         string API_KEY = "f0MastlmVn0YODooYoPfzK3A";
+         string API_SECRET = "48uarB-jI8yOa4S7Qop1DclE6X4kLloimXTYBSqLInHno9fp";
+
+        
+
+        public string GetAPIKEY()
+        {
+            return this.API_KEY;
+        }
+
+        public string GETAPISECRET()
+        {
+            return this.API_SECRET;
+        }
+    }
+        
+    
+}
